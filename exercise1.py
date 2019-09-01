37e14860d30f54162483320fbb92b6c4eba383d1
Roberto Alquicer <alquicer@MacBook-Air-de-Roberto.local>
2019-09-01 12:18:08 -0500

    first commit

---
+++ exercise1.py
@@ -0,0 +1,31 @@
+#Printing hello world
+#print("Hello World")
+
+##Tipos de datos
+cadena = "Hola "
+cadena2 = 'Hola mundo'
+
+#nombre = input("¿Cómo te llamas?\n")
+#resultado = cadena + nombre
+#print(resultado)
+
+uno = 1
+dos = 2
+tres = uno + dos
+#print(tres)
+
+#Listas
+mylist = [1,2,3,4]
+#print(mylist[2])
+for element in mylist:
+    print(element)
+
+#Agregando objetos a listas
+mylist.append(2)
+#print(mylist)
+#print(mylist.count(2))
+#
+
+
+
+
