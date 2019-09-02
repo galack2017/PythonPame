6967cea9d4280165374d8be8bff470d91544fa36
Roberto Alquicer <alquicer@MacBook-Air-de-Roberto.local>
2019-09-01 23:25:06 -0500

    primera clase

---
+++ clase1.py
@@ -0,0 +1,100 @@
+#print('hello')
+
+#uno = 1
+#dos = 2
+#tres = uno + dos
+#cadena = "hola"
+#print(tres)
+
+#nombre=input("Cómo te llamas?\n")
+#print("Hola "+str(uno))
+
+#listas
+#lista = [1,2,3,4]
+#print(lista[1])
+
+#for element in lista:
+#    print(element)
+
+#lista.append(1)
+#print(lista.count(1))
+
+
+#Operadores aritmeticos
+#numero1 = 10
+#numero2 = 20
+
+#numero1+=numero2
+#numero2*=numero1
+
+#Modulo
+#resultado = 4%2
+#print(resultado)
+
+#potencias
+#potencia = 2**9
+
+
+#Calcular la suma de todos los elementos de una lista.
+
+#lista=[2,2,2,2]
+#suma=0
+#for element in lista:
+#    suma+=element
+#    print(suma)
+
+#90
+
+
+#Condicionales
+#if
+#< > >= <=
+
+#if 2 == 2:
+    #print("cierto")
+
+#lista = [1,2,3,4]
+#if lista[2] > lista[1]:
+    #print("el valor "+str(lista[2])+ " es mayor que "+str(lista[3]))
+
+#number = input("dame un numero ")
+
+#Crea un programa que calcule la edad con base en el año de nacimiento de un usuario y verifique si es mayor de edad (+18)
+
+#nacimiento=+int(input("Ingresa tu año de nacimiento:   "))
+#year= 2019
+#year-= nacimiento
+#print(year)
+
+#if year >=18:
+#    print("Eres mayor de edad")
+
+
+#100
+
+#num = 5
+#while num > 1:
+#    print("es mayor")
+#    num-=1
+
+
+#Crear un iterador de listas que pare cuando uno de sus elementos sea 0
+
+lista = [3,4,5,7]
+resta=1
+for element in lista:
+    while element > 0:
+        element -= resta
+        print(element)
+        if element==0:
+            print("Llego a cero")
+
+
+
+
+
+
+
+
+
+
